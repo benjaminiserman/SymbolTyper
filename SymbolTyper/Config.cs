4c5ca81de067c2aac3383ff314c570fda036855d
Benjamin E. Iserman <winggar1228@gmail.com>
2023-05-03 01:24:26 -0500

    remove StringStartKey

--- SymbolTyper/Config.cs
+++ SymbolTyper/Config.cs
@@ -7,6 +7,5 @@ using WK.Libraries.HotkeyListenerNS;
 internal record Config
 {
 	public Hotkey ToggleHotkey { get; set; } = new(Keys.Alt, Keys.S);
-	public Keys StringStartKey { get; set; } = Keys.Alt;
 	public Dictionary<string, string> Replacements { get; set; } = new();
 }
